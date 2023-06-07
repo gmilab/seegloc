96c16c3c7ab9a0165e6d7156f17c93b1d6fa7ce9
Simeon Wong <dtxe@users.noreply.github.com>
2023-06-07 13:32:28 -0400

    update output filenames to improve directory readability

--- coregister_ctmri.sh
+++ coregister_ctmri.sh
@@ -23,16 +23,16 @@ FSLDIR=/usr/local/fsl
 $FSLDIR/bin/flirt -in "$1" -ref "$2" -out "$3/CT_inMRIspace.nii.gz" -omat "$3/transform_CTtoMRI_affine.mat" -bins 256 -cost mutualinfo -searchrx -75 75 -searchry -75 75 -searchrz -75 75 -dof 12
 
 # BET the subject MRI for template registration
-$FSLDIR/bin/bet "$2" "$3/MRI_betted.nii.gz"
+$FSLDIR/bin/bet "$2" "$3/vol_MRI_betted.nii.gz"
 
 # linear transform MRI to template
-$FSLDIR/bin/flirt -ref "$FSLDIR/data/standard/MNI152_T1_1mm_brain.nii.gz" -in "$3/MRI_betted.nii.gz" -out "$3/MRI_inTemplatespace_affineonly.nii.gz" -omat "$3/transform_MRItoTemplate_affine.mat"
+$FSLDIR/bin/flirt -ref "$FSLDIR/data/standard/MNI152_T1_1mm_brain.nii.gz" -in "$3/vol_MRI_betted.nii.gz" -out "$3/vol_MRI_inTemplatespace_affineonly.nii.gz" -omat "$3/transform_MRItoTemplate_affine.mat"
 
 # nonlinear transform MRI to template
-$FSLDIR/bin/fnirt --ref="$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --in="$2" --aff="$3/transform_MRItoTemplate_affine.mat" --fout="$3/transform_MRItoTemplate_fnirt.nii.gz" --iout="$3/MRI_inTemplatespace.nii.gz"
+$FSLDIR/bin/fnirt --ref="$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --in="$2" --aff="$3/transform_MRItoTemplate_affine.mat" --fout="$3/transform_MRItoTemplate_fnirt.nii.gz" --iout="$3/vol_MRI_inTemplatespace.nii.gz"
 
 # applywarp from CT to template
-$FSLDIR/bin/applywarp --ref="$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --in="$1" --out="$3/CT_inTemplatespace.nii.gz" --warp="$3/transform_MRItoTemplate_fnirt.nii.gz" --premat="$3/transform_CTtoMRI_affine.mat"
+$FSLDIR/bin/applywarp --ref="$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --in="$1" --out="$3/vol_CT_inTemplatespace.nii.gz" --warp="$3/transform_MRItoTemplate_fnirt.nii.gz" --premat="$3/transform_CTtoMRI_affine.mat"
 
 # compute affine inverses
 $FSLDIR/bin/convert_xfm -omat "$3/transform_TemplatetoMRI_affine.mat" -inverse "$3/transform_MRItoTemplate_affine.mat"
@@ -43,21 +43,7 @@ $FSLDIR/bin/convert_xfm -omat "$3/transform_TemplatetoCT_affine.mat" -concat "$3
 $FSLDIR/bin/convert_xfm -omat "$3/transform_CTtoTemplate_affine.mat" -concat "$3/transform_MRItoTemplate_affine.mat" "$3/transform_CTtoMRI_affine.mat"
 
 # linearly transform brain mask to CT space
-$FSLDIR/bin/flirt -in "$FSLDIR/data/standard/MNI152_T1_1mm_brain_mask.nii.gz" -ref "$1" -out "$3/brainmask_inCT.nii.gz" -init "$3/transform_TemplatetoCT_affine.mat" -applyxfm
-
-# render sanity check images
-# - CT overlaid on the template
-# - MRI overlaid on the template
-# - MRI transformed into template space
-fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 10 150 --ncols 9 --nrows 3 -of "$3/CT_inTemplate_ax.png"  -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --alpha 100.0 --cmap greyscale "$3/CT_inTemplatespace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
-fsleyes render --scene lightbox --zaxis 1 --sliceSpacing 6.5 --zrange 23 198 --ncols 9 --nrows 3 -of "$3/CT_inTemplate_cor.png" -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --alpha 100.0 --cmap greyscale "$3/CT_inTemplatespace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
-fsleyes render --scene lightbox --zaxis 0 --sliceSpacing 5.0 --zrange 23 160 --ncols 9 --nrows 3 -of "$3/CT_inTemplate_sag.png" -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --alpha 100.0 --cmap greyscale "$3/CT_inTemplatespace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
-
-fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 0 181 --ncols 9 --nrows 3 -of "$3/MRI_inTemplate_overlay.png" -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --overlayType volume --alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap greyscale "$3/MRI_inTemplatespace.nii.gz" --overlayType volume --alpha 25 --brightness 65 --contrast 85 --cmap brain_colours_actc_iso
-fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 0 181 --ncols 9 --nrows 3 -of "$3/MRI_inTemplate_solo.png" -sz 2560 1440 "$3/MRI_inTemplatespace.nii.gz" --overlayType volume --alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap greyscale
-
-fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 10 150 --ncols 9 --nrows 3 -of "$3/CT_inMRI_ax.png"  -sz 2560 1440 "$2" --alpha 100.0 --cmap greyscale "$3/CT_inMRIspace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
-
+$FSLDIR/bin/flirt -in "$FSLDIR/data/standard/MNI152_T1_1mm_brain_mask.nii.gz" -ref "$1" -out "$3/vol_brainmask_inCT.nii.gz" -init "$3/transform_TemplatetoCT_affine.mat" -applyxfm
 
 # write out json file with source filenames
 cat << EOF > $3/coregister_meta.json
@@ -66,3 +52,6 @@ cat << EOF > $3/coregister_meta.json
     "src_mri": "$2"
 }
 EOF
+
+# render QC images
+. ./coregister_ct_mri_renderimage.sh $3
--- coregister_ctmri_renderimages.sh
+++ coregister_ctmri_renderimages.sh
@@ -21,15 +21,15 @@ echo $MRIPATH $CTPATH >> "$1/run_log.txt"
 
 set -x #echo on
 
-# render sanity check images
+# render quality control check images
 # - CT overlaid on the template
 # - MRI overlaid on the template
 # - MRI transformed into template space
-fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 10 150 --ncols 9 --nrows 3 -of "$COREGDIR/CT_inTemplate_ax.png"  -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --alpha 100.0 --cmap greyscale "$COREGDIR/CT_inTemplatespace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
-fsleyes render --scene lightbox --zaxis 1 --sliceSpacing 6.5 --zrange 23 198 --ncols 9 --nrows 3 -of "$COREGDIR/CT_inTemplate_cor.png" -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --alpha 100.0 --cmap greyscale "$COREGDIR/CT_inTemplatespace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
-fsleyes render --scene lightbox --zaxis 0 --sliceSpacing 5.0 --zrange 23 160 --ncols 9 --nrows 3 -of "$COREGDIR/CT_inTemplate_sag.png" -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --alpha 100.0 --cmap greyscale "$COREGDIR/CT_inTemplatespace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
+fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 10 150 --ncols 9 --nrows 3 -of "$COREGDIR/viz_CT_inTemplate_ax.png"  -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --alpha 100.0 --cmap greyscale "$COREGDIR/vol_CT_inTemplatespace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
+fsleyes render --scene lightbox --zaxis 1 --sliceSpacing 6.5 --zrange 23 198 --ncols 9 --nrows 3 -of "$COREGDIR/viz_CT_inTemplate_cor.png" -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --alpha 100.0 --cmap greyscale "$COREGDIR/vol_CT_inTemplatespace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
+fsleyes render --scene lightbox --zaxis 0 --sliceSpacing 5.0 --zrange 23 160 --ncols 9 --nrows 3 -of "$COREGDIR/viz_CT_inTemplate_sag.png" -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --alpha 100.0 --cmap greyscale "$COREGDIR/vol_CT_inTemplatespace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
 
-fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 0 181 --ncols 9 --nrows 3 -of "$COREGDIR/MRI_inTemplate_overlay.png" -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --overlayType volume --alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap greyscale "$COREGDIR/MRI_inTemplatespace.nii.gz" --overlayType volume --alpha 25 --brightness 65 --contrast 85 --cmap brain_colours_actc_iso
-fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 0 181 --ncols 9 --nrows 3 -of "$COREGDIR/MRI_inTemplate_solo.png" -sz 2560 1440 "$COREGDIR/MRI_inTemplatespace.nii.gz" --overlayType volume --alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap greyscale
+fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 0 181 --ncols 9 --nrows 3 -of "$COREGDIR/viz_MRI_inTemplate_overlay.png" -sz 2560 1440 "$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz" --overlayType volume --alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap greyscale "$COREGDIR/vol_MRI_inTemplatespace.nii.gz" --overlayType volume --alpha 25 --brightness 65 --contrast 85 --cmap brain_colours_actc_iso
+fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 0 181 --ncols 9 --nrows 3 -of "$COREGDIR/viz_MRI_inTemplate_solo.png" -sz 2560 1440 "$COREGDIR/vol_MRI_inTemplatespace.nii.gz" --overlayType volume --alpha 100.0 --brightness 50.0 --contrast 50.0 --cmap greyscale
 
-fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 10 150 --ncols 9 --nrows 3 -of "$COREGDIR/CT_inMRI_ax.png"  -sz 2560 1440 "$MRIPATH" --alpha 100.0 --cmap greyscale "$COREGDIR/CT_inMRIspace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
+fsleyes render --scene lightbox --zaxis 2 --sliceSpacing 5.5 --zrange 10 150 --ncols 9 --nrows 3 -of "$COREGDIR/viz_CT_inMRI_ax.png"  -sz 2560 1440 "$MRIPATH" --alpha 100.0 --cmap greyscale "$COREGDIR/vol_CT_inMRIspace.nii.gz" --overlayType volume --alpha 100 --cmap red --displayRange 180.0 2000.0
