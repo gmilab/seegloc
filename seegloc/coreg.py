1428662f4aced3b6c70e21b8d985daf92bae5f7d
Simeon Wong <dtxe@users.noreply.github.com>
2024-07-08 16:50:53 -0400

    update coreg.py: enable partial pipeline execution

--- seegloc/coreg.py
+++ seegloc/coreg.py
@@ -2,17 +2,13 @@ import os
 import argparse
 import logging
 import datetime
-
-from fsl.wrappers import flirt, fnirt, bet, applywarp, invxfm, concatxfm, applyxfm
 import json
+from typing import Optional
 
-try:
-    from fsleyes.render import main as fsleyes_render
-except ImportError:
-    from .fsleyes_cli import render as fsleyes_render
+FSLDIR = os.environ['FSLDIR']
 
 
-def main():
+def pipeline_full():
     parser = argparse.ArgumentParser(
         description=
         'Coregister subject CT to subject MRI to template MRI with FSL, then generate quality-control images.',
@@ -26,15 +22,14 @@ def main():
     parser.add_argument('coreg_output',
                         help='Path to coregistration output directory',
                         type=str)
-    parser.add_argument('--silent', '-s',
+    parser.add_argument('--silent',
+                        '-s',
                         help='Hide info messages.',
                         action='store_true')
-    parser.add_argument(
-        '--skip-slow',
-        dest='testing',
-        help=
-        'Skip time-consuming steps where output already exists. Useful for troubleshooting later steps.',
-        action='store_true')
+    parser.add_argument('--run-step',
+                        type=int,
+                        default=None,
+                        help='Run only the specified step')
 
     args = parser.parse_args()
 
@@ -50,113 +45,142 @@ def main():
     if not os.path.exists(args.coreg_output):
         os.makedirs(args.coreg_output)
 
+
+    coreg_fsl(args.subject_mri, args.subject_ct, args.coreg_output,
+                  args.run_step)
+
+    # write json file with source filenames
+    with open(os.path.join(args.coreg_output, 'coregister_meta.json'),
+              'w') as f:
+        json.dump(
+            {
+                'src_ct': os.path.abspath(args.subject_ct),
+                'src_mri': os.path.abspath(args.subject_mri),
+            }, f)
+
+    generate_qc(args.subject_mri, args.coreg_output)
+
+    logging.info(
+        f'Coregistration completed in {(datetime.datetime.now() - t1).total_seconds()} s'
+    )
+
+
+def coreg_fsl(subject_mri: str,
+              subject_ct: str,
+              coreg_output: str,
+              run_step: Optional[int] = None):
+    from fsl.wrappers import flirt, fnirt, bet, applywarp, invxfm, concatxfm, applyxfm
+
     def crd(x: str) -> str:
         '''Return path to coregistration output file'''
-        return os.path.join(args.coreg_output, x)
-
-    fsldir = os.environ['FSLDIR']
+        return os.path.join(coreg_output, x)
 
     # CT to MRI affine
-    logging.info('[1/10] flirt: CT → MRI')
-    if not args.testing or not os.path.exists(
-            crd('transform_CTtoMRI_affine.mat')):
+    if run_step == 1 or (run_step == None):
+        logging.info('[1/10] flirt: CT → MRI')
         flirt(
-            src=args.subject_ct,
-            ref=args.subject_mri,
+            src=subject_ct,
+            ref=subject_mri,
             out=crd('vol_CT_inMRIspace.nii.gz'),
             omat=crd('transform_CTtoMRI_affine.mat'),
-            bins=256,
+            bins=64,
             cost='mutualinfo',
-            dof=12,
-            searchrx=[-75, 75],
-            searchry=[-75, 75],
-            searchrz=[-75, 75],
+            dof=6,
+            searchrx=[-45, 45],
+            searchry=[-45, 45],
+            searchrz=[-45, 45],
         )
 
     # BET the subject MRI for template registration
-    logging.info('[2/10] bet: subject MRI')
-    if not args.testing or not os.path.exists(crd('vol_MRI_betted.nii.gz')):
+    if run_step == 2 or (run_step == None):
+        logging.info('[2/10] bet: subject MRI')
         bet(
-            args.subject_mri,
+            subject_mri,
             crd('vol_MRI_betted.nii.gz'),
         )
 
     # MRI to template affine
-    logging.info('[3/10] flirt: MRI → template')
-    if not args.testing or not os.path.exists(
-            crd('vol_MRI_inTemplatespace_affineonly.nii.gz')):
+    if run_step == 3 or (run_step == None):
+        logging.info('[3/10] flirt: MRI → template')
         flirt(
             src=crd('vol_MRI_betted.nii.gz'),
-            ref=f'{fsldir}/data/standard/MNI152_T1_1mm_brain.nii.gz',
+            ref=f'{FSLDIR}/data/standard/MNI152_T1_1mm_brain.nii.gz',
             out=crd('vol_MRI_inTemplatespace_affineonly.nii.gz'),
             omat=crd('transform_MRItoTemplate_affine.mat'),
         )
 
     # MRI to template nonlinear
-    logging.info('[4/10] fnirt: MRI → template nonlinear')
-    if not args.testing or not os.path.exists(
-            crd('transform_MRItoTemplate_fnirt.nii.gz')):
+    if run_step == 4 or (run_step == None):
+        logging.info('[4/10] fnirt: MRI → template nonlinear')
         fnirt(
-            src=args.subject_mri,
-            ref=f'{fsldir}/data/standard/MNI152_T1_1mm.nii.gz',
+            src=subject_mri,
+            ref=f'{FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz',
             iout=crd('vol_MRI_inTemplatespace.nii.gz'),
             fout=crd('transform_MRItoTemplate_fnirt.nii.gz'),
             aff=crd('transform_MRItoTemplate_affine.mat'),
         )
 
     # applywarp from CT to template
-    logging.info('[5/10] applywarp: CT → template')
-    if not args.testing or not os.path.exists(
-            crd('vol_CT_inTemplatespace.nii.gz')):
+    if run_step == 5 or (run_step == None):
+        logging.info('[5/10] applywarp: CT → template')
         applywarp(
-            ref=f'{fsldir}/data/standard/MNI152_T1_1mm.nii.gz',
-            src=args.subject_ct,
+            ref=f'{FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz',
+            src=subject_ct,
             out=crd('vol_CT_inTemplatespace.nii.gz'),
             warp=crd('transform_MRItoTemplate_fnirt.nii.gz'),
             premat=crd('transform_CTtoMRI_affine.mat'),
         )
 
     # compute inverse affines
-    logging.info('[6/10] invxfm: MRI → CT')
-    invxfm(
-        inmat=crd('transform_MRItoTemplate_affine.mat'),
-        omat=crd('transform_TemplatetoMRI_affine.mat'),
-    )
-    logging.info('[7/10] invxfm: CT → MRI')
-    invxfm(
-        inmat=crd('transform_CTtoMRI_affine.mat'),
-        omat=crd('transform_MRItoCT_affine.mat'),
-    )
+    if run_step == 6 or (run_step == None):
+        logging.info('[6/10] invxfm: MRI → CT')
+        invxfm(
+            inmat=crd('transform_MRItoTemplate_affine.mat'),
+            omat=crd('transform_TemplatetoMRI_affine.mat'),
+        )
+    if run_step == 6 or (run_step == None):
+        logging.info('[7/10] invxfm: CT → MRI')
+        invxfm(
+            inmat=crd('transform_CTtoMRI_affine.mat'),
+            omat=crd('transform_MRItoCT_affine.mat'),
+        )
 
     # compound affines
-    logging.info('[8/10] concatxfm: template → CT')
-    concatxfm(
-        atob=crd('transform_TemplatetoMRI_affine.mat'),
-        btoc=crd('transform_MRItoCT_affine.mat'),
-        atoc=crd('transform_TemplatetoCT_affine.mat'),
-    )
-    logging.info('[9/10] concatxfm: CT → template')
-    concatxfm(
-        atob=crd('transform_CTtoMRI_affine.mat'),
-        btoc=crd('transform_MRItoTemplate_affine.mat'),
-        atoc=crd('transform_CTtoTemplate_affine.mat'),
-    )
+    if run_step == 8 or (run_step == None):
+        logging.info('[8/10] concatxfm: template → CT')
+        concatxfm(
+            atob=crd('transform_TemplatetoMRI_affine.mat'),
+            btoc=crd('transform_MRItoCT_affine.mat'),
+            atoc=crd('transform_TemplatetoCT_affine.mat'),
+        )
+    if run_step == 9 or (run_step == None):
+        logging.info('[9/10] concatxfm: CT → template')
+        concatxfm(
+            atob=crd('transform_CTtoMRI_affine.mat'),
+            btoc=crd('transform_MRItoTemplate_affine.mat'),
+            atoc=crd('transform_CTtoTemplate_affine.mat'),
+        )
 
     # transform brain mask to CT space
-    logging.info('[10/10] applyxfm: brain mask → CT space')
-    applyxfm(
-        src=f'{fsldir}/data/standard/MNI152_T1_1mm_brain_mask.nii.gz',
-        ref=args.subject_ct,
-        mat=crd('transform_TemplatetoCT_affine.mat'),
-        out=crd('vol_brainmask_inCT.nii.gz'),
-    )
+    if run_step == 10 or (run_step == None):
+        logging.info('[10/10] applyxfm: brain mask → CT space')
+        applyxfm(
+            src=f'{FSLDIR}/data/standard/MNI152_T1_1mm_brain_mask.nii.gz',
+            ref=subject_ct,
+            mat=crd('transform_TemplatetoCT_affine.mat'),
+            out=crd('vol_brainmask_inCT.nii.gz'),
+        )
 
-    # write json file with source filenames
-    with open(crd('coregister_meta.json'), 'w') as f:
-        json.dump({
-            'src_ct': args.subject_ct,
-            'src_mri': args.subject_mri,
-        }, f)
+
+def generate_qc(subject_mri: str, coreg_output: str):
+    try:
+        from fsleyes.render import main as fsleyes_render
+    except ImportError:
+        from .fsleyes_cli import render as fsleyes_render
+
+    def crd(x: str) -> str:
+        '''Return path to coregistration output file'''
+        return os.path.join(coreg_output, x)
 
     # generate QC images
     # - CT overlaid on the template
@@ -166,7 +190,7 @@ def main():
         '--scene', 'lightbox', '--zaxis', '2', '--sliceSpacing', '5.5',
         '--zrange', '10', '150', '--ncols', '9', '--nrows', '3', '-of',
         crd("vis_CT_inTemplate_ax.png"), '-sz', '2560', '1440',
-        f'{fsldir}/data/standard/MNI152_T1_1mm.nii.gz', '--alpha', '100.0',
+        f'{FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz', '--alpha', '100.0',
         '--cmap', 'greyscale',
         crd("vol_CT_inTemplatespace.nii.gz"), '--overlayType', 'volume',
         '--alpha', '100', '--cmap', 'red', '--displayRange', '180.0', '2000.0'
@@ -175,7 +199,7 @@ def main():
         '--scene', 'lightbox', '--zaxis', '1', '--sliceSpacing', '6.5',
         '--zrange', '23', '198', '--ncols', '9', '--nrows', '3', '-of',
         crd("vis_CT_inTemplate_cor.png"), '-sz', '2560', '1440',
-        f'{fsldir}/data/standard/MNI152_T1_1mm.nii.gz', '--alpha', '100.0',
+        f'{FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz', '--alpha', '100.0',
         '--cmap', 'greyscale',
         crd("vol_CT_inTemplatespace.nii.gz"), '--overlayType', 'volume',
         '--alpha', '100', '--cmap', 'red', '--displayRange', '180.0', '2000.0'
@@ -184,7 +208,7 @@ def main():
         '--scene', 'lightbox', '--zaxis', '0', '--sliceSpacing', '5.0',
         '--zrange', '23', '160', '--ncols', '9', '--nrows', '3', '-of',
         crd("vis_CT_inTemplate_sag.png"), '-sz', '2560', '1440',
-        f'{fsldir}/data/standard/MNI152_T1_1mm.nii.gz', '--alpha', '100.0',
+        f'{FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz', '--alpha', '100.0',
         '--cmap', 'greyscale',
         crd("vol_CT_inTemplatespace.nii.gz"), '--overlayType', 'volume',
         '--alpha', '100', '--cmap', 'red', '--displayRange', '180.0', '2000.0'
@@ -196,7 +220,7 @@ def main():
         '--scene', 'lightbox', '--zaxis', '2', '--sliceSpacing', '5.5',
         '--zrange', '0', '181', '--ncols', '9', '--nrows', '3', '-of',
         crd("vis_MRI_inTemplate_overlay.png"), '-sz', '2560', '1440',
-        f'{fsldir}/data/standard/MNI152_T1_1mm.nii.gz', '--overlayType',
+        f'{FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz', '--overlayType',
         'volume', '--alpha', '100.0', '--brightness', '50.0', '--contrast',
         '50.0', '--cmap', 'greyscale',
         crd('vol_MRI_inTemplatespace.nii.gz'), '--overlayType', 'volume',
@@ -207,7 +231,7 @@ def main():
         '--scene', 'lightbox', '--zaxis', '2', '--sliceSpacing', '5.5',
         '--zrange', '0', '181', '--ncols', '9', '--nrows', '3', '-of',
         crd("vis_MRI_inTemplate_overlay.png"), '-sz', '2560', '1440',
-        f'{fsldir}/data/standard/MNI152_T1_1mm.nii.gz', '--overlayType',
+        f'{FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz', '--overlayType',
         'volume', '--alpha', '100.0', '--brightness', '50.0', '--contrast',
         '50.0', '--cmap', 'greyscale',
         crd('vol_MRI_inTemplatespace.nii.gz'), '--overlayType', 'volume',
@@ -220,16 +244,12 @@ def main():
     fsleyes_render([
         '--scene', 'lightbox', '--zaxis', '2', '--sliceSpacing', '5.5',
         '--zrange', '10', '150', '--ncols', '9', '--nrows', '3', '-of',
-        crd('vis_CT_inMRI_ax.png'), '-sz', '2560', '1440', args.subject_mri,
+        crd('vis_CT_inMRI_ax.png'), '-sz', '2560', '1440', subject_mri,
         '--alpha', '100.0', '--cmap', 'greyscale',
         crd('vol_CT_inMRIspace.nii.gz'), '--overlayType', 'volume', '--alpha',
         '100', '--cmap', 'red', '--displayRange', '180.0', '2000.0'
     ])
 
-    logging.info(
-        f'Coregistration completed in {(datetime.datetime.now() - t1).total_seconds()} s'
-    )
-
 
 if __name__ == '__main__':
-    main()
+    pipeline_full()
