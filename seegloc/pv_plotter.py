fa80de2b73946486b4bff034b45dac64bc2bdb77
Simeon Wong <dtxe@users.noreply.github.com>
2024-10-17 15:23:06 -0400

    enable skipping static images or video rendering

--- seegloc/pv_plotter.py
+++ seegloc/pv_plotter.py
@@ -24,6 +24,8 @@ def get_plotter(
         save_base: str,
         off_screen: bool = True,
         window_size: Tuple[int, int] = (3008, 1808),
+        write_png: bool = True,
+        write_video: bool = True,
 ) -> pv.Plotter:
     ''' Create a pyvista plotter with a white background and terrain style. 
     Save the plotter to disk as pngs in sagittal, coronal, and axial views.
@@ -46,49 +48,51 @@ def get_plotter(
         cam_dist = np.linalg.norm(np.array(cam_pos) -
                                   np.array(cam_focal)) * 0.4
 
-        # saggital view
-        plotter.view_vector([-1, 0, 0])
-        pvzoom(plotter)
-        plotter.render()
-        plotter.screenshot(f'{save_base}_sag.png', transparent_background=True)
+        if write_png
+            # saggital view
+            plotter.view_vector([-1, 0, 0])
+            pvzoom(plotter)
+            plotter.render()
+            plotter.screenshot(f'{save_base}_sag.png', transparent_background=True)
 
-        # coronal view
-        plotter.view_vector([0, 1, 0])
-        pvzoom(plotter)
-        plotter.render()
-        plotter.screenshot(f'{save_base}_cor.png', transparent_background=True)
+            # coronal view
+            plotter.view_vector([0, 1, 0])
+            pvzoom(plotter)
+            plotter.render()
+            plotter.screenshot(f'{save_base}_cor.png', transparent_background=True)
 
-        # axial view
-        plotter.view_vector([0, 0, 1])
-        pvzoom(plotter)
-        plotter.render()
-        plotter.screenshot(f'{save_base}_ax.png', transparent_background=True)
+            # axial view
+            plotter.view_vector([0, 0, 1])
+            pvzoom(plotter)
+            plotter.render()
+            plotter.screenshot(f'{save_base}_ax.png', transparent_background=True)
 
-        plotter.open_movie(f'{save_base}_mov.mp4')
-        plotter.camera.SetClippingRange((0.1, 1000))
+        if write_video:
+            plotter.open_movie(f'{save_base}_mov.mp4')
+            plotter.camera.SetClippingRange((0.1, 1000))
 
-        # get the Z bounds of the first actor (eg. ct volume)
-        z_bounds = plotter.actors[list(plotter.actors.keys())[0]].bounds[4:6]
-        z_range = z_bounds[1] - z_bounds[0]
+            # get the Z bounds of the first actor (eg. ct volume)
+            z_bounds = plotter.actors[list(plotter.actors.keys())[0]].bounds[4:6]
+            z_range = z_bounds[1] - z_bounds[0]
 
-        # path
-        steps = np.arange(0, 360, 4)
-        path = [
-            np.vstack((cam_dist * np.cos(np.deg2rad(steps)),
-                       cam_dist * np.sin(np.deg2rad(steps)),
-                       np.ones_like(steps) * i * z_range)).T
-            for i in [-0.9, 0, 0.9]
-        ]
-        path = np.vstack(path)
-        path = path + np.array(cam_focal)
+            # path
+            steps = np.arange(0, 360, 4)
+            path = [
+                np.vstack((cam_dist * np.cos(np.deg2rad(steps)),
+                        cam_dist * np.sin(np.deg2rad(steps)),
+                        np.ones_like(steps) * i * z_range)).T
+                for i in [-0.9, 0, 0.9]
+            ]
+            path = np.vstack(path)
+            path = path + np.array(cam_focal)
 
-        plotter.camera.SetFocalPoint(cam_focal)
-        plotter.camera.SetViewUp(cam_normal)
-        plotter.camera.SetClippingRange((0.01, 1000))
+            plotter.camera.SetFocalPoint(cam_focal)
+            plotter.camera.SetViewUp(cam_normal)
+            plotter.camera.SetClippingRange((0.01, 1000))
 
-        for i in tqdm(path, desc='Rendering frames...'):
-            plotter.camera.SetPosition(i)
-            plotter.write_frame()
+            for i in tqdm(path, desc='Rendering frames...'):
+                plotter.camera.SetPosition(i)
+                plotter.write_frame()
 
     finally:
         plotter.close()
